331fdd18bb88344c724a3f7f54f6fa9a9d16a982
Yourlockhead <bryantmward@gmail.com>
2017-04-04 21:35:54 -0400

    Initial commit with .net core and dll

---
+++ CanvacoreLib/Canvas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CanvacoreLib
+{
+    public class Canvas
+    {
+        List<Pixel> Pixels = new List<Pixel>();
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ CanvacoreLib/Pixel.cs
@@ -0,0 +1,10 @@
+﻿namespace CanvacoreLib
+{
+    public class Pixel
+    {
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+        public int A { get; set; }
+    }
+}
